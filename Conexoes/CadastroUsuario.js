e3c58267637f3d501e5211b3ff5f9b4380964a91
GuiNegr <GuiNegr.git@outlook.com>
2024-09-21 02:34:00 -0300

    Add: lista usuarios

--- Conexoes/CadastroUsuario.js
+++ Conexoes/CadastroUsuario.js
@@ -1,45 +1,90 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById('cadastroForm');
+    
     if (form) {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
+  
+        const userToEdit = localStorage.getItem('userEdit');
+        
+        if (userToEdit) {
+        
+            const user = JSON.parse(userToEdit);
+            document.getElementById('nome').value = user.usuaNmUsuario;
+            document.getElementById('email').value = user.usuaDsEmail;
+            document.getElementById('cpf').value = user.usuaDsCPF;
+            document.getElementById('senha').value = user.usuaDsPassword;
+ 
+       
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
 
-            const nome = document.getElementById('nome').value;
-            const email = document.getElementById('email').value;
-            const senha = document.getElementById('senha').value;
-            const cpf = document.getElementById('cpf').value;
+                const nome = document.getElementById('nome').value;
+                const email = document.getElementById('email').value;
+                const senha = document.getElementById('senha').value;
+                const cpf = document.getElementById('cpf').value;
+                const tipoUsuario = document.getElementById('tipoUsuario').value;
 
-            const dados = {
-                usuaNmUsuario: nome,
-                usuaDsEmail: email,
-                usuaDsCPF: cpf,
-                usuaDsPassword: senha
-            };
+                const dados = {
+                    usuaNmUsuario: nome,
+                    usuaDsCPF: cpf,
+                    usuaDsPassword: senha ,
+                    usuaCdGrupo: tipoUsuario
+                };
 
-            fetch('http://localhost:8015/user', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(dados)
-            })
-            .then(response => {
-                console.log('Resposta recebida:', response);
-                if (!response.ok) {
-                    throw new Error(`Erro ${response.status}: ${response.statusText}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Dados recebidos:', data);
-                alert('Cadastro realizado com sucesso!');
-                window.location.href = 'telaPrincipal.html';
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                alert('Ocorreu um erro ao realizar o cadastro.');
+                fetch(`http://localhost:8015/user/${user.idUsuario}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dados)
+                })
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    localStorage.removeItem('userEdit'); 
+                    window.location.href = 'ListaUsuario.html';;
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    alert('Ocorreu um erro ao atualizar o usuário.');
+                });
             });
-        });
+        } else {
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+
+                const nome = document.getElementById('nome').value;
+                const email = document.getElementById('email').value;
+                const senha = document.getElementById('senha').value;
+                const cpf = document.getElementById('cpf').value;
+                const tipoUsuario = document.getElementById('tipoUsuario').value;
+
+                const dados = {
+                    usuaNmUsuario: nome,
+                    usuaDsEmail: email,
+                    usuaDsCPF: cpf,
+                    usuaDsPassword: senha,
+                    usuaCdGrupo: tipoUsuario
+                };
+
+                fetch('http://localhost:8015/user', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dados)
+                })
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    window.location.href = 'ListaUsuario.html';
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                });
+            });
+        }
     } else {
         console.error('Formulário não encontrado!');
     }
